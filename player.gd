9e936b169cf0eff58870b35fe22407cc2ab2f424
Jackson Gee <jacksongee2005@gmail.com>
2026-07-11 13:05:11 +0800

    Fixed Collisions and team sprite hotswap

--- player.gd
+++ player.gd
@@ -1,8 +1,34 @@
 extends CharacterBody2D
 
-var speed = 300
+enum Teams {
+	ATTACKER,
+	DEFENDER
+}
 
-func _physics_process(delta):
+@onready var sprite: Sprite2D = $Sprite2D
+
+var speed = 300
+var health = 100
+@export var team: Teams = Teams.ATTACKER
+
+@export var attacker_texture: Texture2D = preload("res://assets/player/attacker.png")
+@export var defender_texture: Texture2D = preload("res://assets/player/defender.png")
+
+
+func _ready():
+	update_team_sprite()
+
+
+func update_team_sprite():
+	match team:
+		Teams.ATTACKER:
+			sprite.texture = attacker_texture
+
+		Teams.DEFENDER:
+			sprite.texture = defender_texture
+
+
+func _physics_process(_delta):
 	var direction = Vector2.ZERO
 
 	if Input.is_action_pressed("move_right"):
@@ -22,4 +48,5 @@ func _physics_process(delta):
 
 	velocity = direction * speed
 	move_and_slide()
+
 	look_at(get_global_mouse_position())
